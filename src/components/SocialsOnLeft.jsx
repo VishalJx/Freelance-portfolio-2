87bb743d1af2ab76f4825ddac57e2fe1a3f83bc7
VishalJx <itsmevishal360@gmail.com>
2024-12-27 15:35:12 +0530

    feature:hover on social icons

--- src/components/SocialsOnLeft.jsx
+++ src/components/SocialsOnLeft.jsx
@@ -8,9 +8,15 @@ const SocialsOnLeft = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-end
     gap-9">
-    <FaInstagram className="text-2xl fixed bottom-[17.3rem]"/>
-    <FaLinkedinIn className="text-2xl fixed bottom-[13.3rem]"/>
-    <FaGithub className="text-2xl fixed bottom-[9.3rem]"/>
+      <a href="https://www.instagram.com/" className='flex justify-center'>
+        <FaInstagram className="text-2xl fixed bottom-[17.3rem] hover:text-primary-100 hover:shadow-lg duration-200"/>
+      </a>
+      <a href="https://www.linkedin.com/" className='flex justify-center'>
+        <FaLinkedinIn className="text-2xl fixed bottom-[12.3rem] hover:text-primary-100 hover:shadow-lg duration-200" />
+      </a>
+      <a href="" className='flex justify-center'>
+        <FaGithub className="text-2xl fixed bottom-[7.3rem] hover:text-primary-100 hover:shadow-lg duration-200"/>
+      </a>
       <div className="w-[2px] h-32 mt-7 bg-secondary-500"></div>
     </div>
   )
