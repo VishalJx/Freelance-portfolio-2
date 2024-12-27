16cf89617fad6f0b2b7761ff8a2e0040db20ffc8
VishalJx <itsmevishal360@gmail.com>
2024-12-27 16:10:35 +0530

    feature:add data.js

---
+++ src/utils/data.js
@@ -0,0 +1,101 @@
+const data = [
+    profilePic="/assets/profile-pic.jpg",
+    homePage={
+        name: "Priya Pal",
+        description: "A Result-Oriented Web Developer building and managing Websites and Web Applications that leads to the success of the overall product"
+    },
+    aboutPage={
+        description: [
+            "I'm a Frontend Focused Web Developer building and managing the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.",
+            "I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community. Feel free to Connect or Follow me on my Linkedin and Instagram where I post useful content related to Web Development and Programming",
+            "I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me."
+        ],
+        skills: [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Node.js",
+            "MongoDB",
+            "Express.js",
+            "Firebase",
+            "Figma",
+            "Adobe XD",
+        ]
+    },
+    publicationsPage=[
+        {
+            title: "A Novel Approach for Image Steganography using Cryptography",
+            description: "This paper presents a novel approach for image steganography using cryptography. The proposed method uses a combination of cryptography and steganography techniques to hide the secret message within the cover image. The secret message is encrypted using a symmetric key algorithm, and the encrypted message is embedded into the cover image using the least significant bit (LSB) substitution technique. The experimental results show that the proposed method provides a high level of security and robustness against various attacks.",
+            date: "Published on 15th June 2021",
+            link: "https://www.researchgate.net/publication/352732186_A_Novel_Approach_for_Image_Steganography_using_Cryptography",
+            img: "/public/assets/project-1.png",
+        },
+        {
+            title: "A Survey on Image Steganography Techniques",
+            description: "This paper presents a survey on image steganography techniques. Image steganography is a method of hiding secret information within an image. The survey covers various image steganography techniques, including LSB substitution, spatial domain techniques, transform domain techniques, and spread spectrum techniques. The paper also discusses the advantages and disadvantages of each technique and provides a comparison of different image steganography methods.",
+            date: "Published on 25th May 2021",
+            link: "https://www.researchgate.net/publication/352732186_A_Novel_Approach_for_Image_Steganography_using_Cryptography",
+            img: "https://www.image.com/image.jpg",
+        },
+        {
+            title: "A Comparative Study of Image Steganography Techniques",
+            description: "This paper presents a comparative study of image steganography techniques. Image steganography is a method of hiding secret information within an image. The study compares various image steganography techniques, including LSB substitution, spatial domain techniques, transform domain techniques, and spread spectrum techniques. The paper evaluates the performance of each technique based on factors such as imperceptibility, capacity, robustness, and security.",
+            date: "Published on 10th May 2021",
+            link: "https://www.researchgate.net/publication/352732186_A_Novel_Approach_for_Image_Steganography_using_Cryptography",
+            img: "https://www.image.com/image.jpg",
+        }
+    ],
+    proejctsPage=[
+        {
+            title: "Speech Transcription with User Diarization",
+            description: "Speech transcription system with user diarization that leverages machine learning technologies to provide precise, speaker-labeled transcripts. By integrating OpenAI's Whisper and advanced speaker recognition techniques, the project delivers high-quality transcription that can distinguish between multiple speakers in complex audio environments.",
+            techStack: ["Fast API", "Python", "NExtJs"],
+            img: "/assets/project-1.png",
+        },
+        {
+            title: "Real-Time Object Detection with YOLOv4",    
+            description: "Real-time object detection system that uses the YOLOv4 model to detect and classify objects in images and videos. The project leverages the power of deep learning and computer vision to provide accurate and efficient object detection capabilities that can be used in a wide range of applications.",
+            techStack: ["YOLOv4", "Python", "OpenCV"],
+            img: "/public/assets/project-2.png",
+        },
+        {
+            title: "Sentiment Analysis with BERT",
+            description: "Sentiment analysis system that uses the BERT model to analyze and classify text data based on sentiment. The project leverages the power of natural language processing and deep learning to provide accurate sentiment analysis capabilities that can be used in a wide range of applications.",
+            techStack: ["BERT", "Python", "TensorFlow"],
+            img: "/public/assets/project-3.png",
+        },
+        {
+            title: "Chatbot with GPT-3",
+            description: "Chatbot system that uses the GPT-3 model to generate human-like responses to user queries. The project leverages the power of natural language processing and deep learning to provide conversational capabilities that can be used in a wide range of applications.",
+            techStack: ["GPT-3", "Python", "Flask"],
+            img: "/public/assets/project-4.png",
+        },
+        {
+            title: "Image Segmentation with U-Net",
+            description: "Image segmentation system that uses the U-Net model to segment and classify objects in images. The project leverages the power of deep learning and computer vision to provide accurate image segmentation capabilities that can be used in a wide range of applications.",
+            techStack: ["U-Net", "Python", "TensorFlow"],
+            img: "/public/assets/project-5.png",
+        },
+        {
+            title: "Recommendation System with Filtering",
+            description: "Recommendation system that uses collaborative filtering to provide personalized recommendations to users. The project leverages the power of machine learning and data mining to provide accurate and efficient recommendation capabilities that can be used in a wide range of applications.",
+            techStack: ["Collaborative Filtering", "Python", "Scikit-learn"],
+            img: "/public/assets/project-6.png",
+        }
+    ],
+    socialLinks=[
+        {
+            name: "Linkedin",
+            link: "https://www.linkedin.com/in/priya-pal/",
+        },
+        {
+            name: "Instagram",
+            link: "https://www.instagram.com/priya_pal/",
+        },
+        {
+            name: "Github",
+            link: "",
+        },
+    ]
+]
